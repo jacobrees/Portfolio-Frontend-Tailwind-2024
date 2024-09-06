0ddcee8518c0d9a56785269c5b0d096650c4ce5d
jacobrees <jacob.rees97@yahoo.com>
2024-09-06 20:12:45 +0100

    Create navigation for portfolio site

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-import './style.css'
+import "./style.css";
 
+const openNavButton = document.querySelector(".navigation-menu");
+const mobileNav = document.querySelector(".mobile-nav");
+const closeNavButton = document.querySelector(".mobile-close-navbar");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.add("translate-x-[-100%]");
+  });
+});
+
+openNavButton.addEventListener("click", () => {
+  mobileNav.classList.remove("translate-x-[-100%]");
+});
+
+closeNavButton.addEventListener("click", () => {
+  mobileNav.classList.add("translate-x-[-100%]");
+});
