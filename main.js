5e4c836940b902649790fc0132b75be1621cffce
jacobrees <jacob.rees97@yahoo.com>
2024-09-06 20:57:31 +0100

    Made it so the mobile nav isnt visible on page load

--- main.js
+++ main.js
@@ -8,13 +8,22 @@ const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 mobileNavLinks.forEach((link) => {
   link.addEventListener("click", () => {
     mobileNav.classList.add("translate-x-[-100%]");
+    setTimeout(() => {
+      mobileNav.classList.add("hidden");
+    }, 500); // Wait for the transition to finish before hiding
   });
 });
 
 openNavButton.addEventListener("click", () => {
-  mobileNav.classList.remove("translate-x-[-100%]");
+  mobileNav.classList.remove("hidden");
+  setTimeout(() => {
+    mobileNav.classList.remove("translate-x-[-100%]");
+  }, 10); // Small delay to ensure the hidden class is removed before starting transition
 });
 
 closeNavButton.addEventListener("click", () => {
   mobileNav.classList.add("translate-x-[-100%]");
+  setTimeout(() => {
+    mobileNav.classList.add("hidden");
+  }, 500); // Wait for the transition to finish before hiding
 });
